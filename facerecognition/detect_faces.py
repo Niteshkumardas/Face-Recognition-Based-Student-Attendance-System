6384c1cd2f7a9eda7b28b9e1aca90f0ff31cf3f0
Adityakafle <jaishiaditya2000@gmail.com>
2022-02-19 15:27:25 +0545

    first commit

---
+++ facerecognition/detect_faces.py
@@ -0,0 +1,30 @@
+# calculate a face embedding for each face in the dataset using facenet
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+from keras.models import load_model
+
+
+
+# get the face embedding for one face
+def get_embedding(model, face_pixels):
+	# scale pixel values
+	face_pixels = face_pixels.astype('float32')
+	# standardize pixel values across channels (global)
+	mean, std = face_pixels.mean(), face_pixels.std()
+	face_pixels = (face_pixels - mean) / std
+	# transform face into one sample
+	samples = expand_dims(face_pixels, axis=0)
+	# make prediction to get embedding
+	yhat = model.predict(samples)
+	return yhat[0]
+
+data = load('dataset_of_students.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
+
+# load the facenet model
+model = load_model('facenet_keras.h5')
+print('Loaded Model')
+
---
+++ facerecognition/model load.py
@@ -0,0 +1,5 @@
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+from keras.models import load_model
